e9d1dd6b633daa74b571d59ce7dbeb646aadd581
henok zelalem <henok.zelalem@adey-meselesh.de>
2022-06-01 09:17:36 +0300

    minor fix

--- route/banner.js
+++ route/banner.js
@@ -1,68 +1,58 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const fs = require('fs');
-const multer = require('multer');
+const fs = require("fs");
+const multer = require("multer");
 
-const mongoose = require('mongoose');
-const { admin, editor } = require('./super');
-const EcaBanner = require('../models/banners-model')
+const mongoose = require("mongoose");
+const { admin, editor } = require("./super");
+const EcaBanner = require("../models/banners-model");
 
 // new Date().toISOString()
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(
-      null,
-      path.join(__dirname, '..', 'banners')
-    );
+    cb(null, path.join(__dirname, "..", "banners"));
   },
   filename: function (req, file, cb) {
     const now = new Date().toISOString();
-    const date = now.replace(/:/g, '-');
+    const date = now.replace(/:/g, "-");
     cb(null, date + file.originalname);
   },
 });
 const upload = multer({ storage: storage });
 
 app.post(
-  '/add-banner',
+  "/add-banner",
   //  editor,
-  upload.single('bannerImage'),
+  upload.single("bannerImage"),
   async (req, res) => {
-    console.log('req.body')
+    console.log("req.body");
     const banner = new EcaBanner({
       bannerImage: req.file.filename,
-      titleAm:req.body.titleAm,
-      titleEn:req.body.titleEn,
-
+      titleAm: req.body.titleAm,
+      titleEn: req.body.titleEn,
     });
     const result = await banner.save();
+    console.log(result);
     res.send(result);
   }
 );
 
-app.post('/get-banners', async (req, res) => {
+app.post("/get-banners", async (req, res) => {
   const banners = await EcaBanner.find();
 
- 
   res.send(banners);
 });
 
 app.post(
-  '/edit-live-promo', editor,
-  upload.single('promoImage'),
+  "/edit-live-promo",
+  editor,
+  upload.single("promoImage"),
   async (req, res) => {
-    const file = path.join(
-      __dirname,
-      '..',
-      'uploads',
-      req.body.oldPromoImage
-    );
-    const promo = await EcaBanner.updateOne(
-      {
-        $set: { promoImage: req.file.filename },
-      }
-    );
+    const file = path.join(__dirname, "..", "uploads", req.body.oldPromoImage);
+    const promo = await EcaBanner.updateOne({
+      $set: { promoImage: req.file.filename },
+    });
     if (promo.nModified) {
       fs.unlink(file, (err) => {
         if (err) res.status(500).send(err);
